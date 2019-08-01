eda82611ee839401341377326d0d00a796f15d66
Debajyoti Das <debo2696@gmail.com>
2019-08-02 01:57:47 +0530

    Update and rename pyramids1 to pyramids1.cs

--- pyramids1.cs
+++ pyramids1.cs
@@ -1,8 +1,8 @@
-/*1>*           2>     A
-   * *                ABA
-  * * *              ABCBA
- * * * *            ABCDCBA
-* * * * *          ABCDEDCBA */
+/*1>  *           2>     A
+     * *                ABA
+    * * *              ABCBA
+   * * * *            ABCDCBA
+  * * * * *          ABCDEDCBA */
 using System;
 using System.Collections.Generic;
 using System.Linq;
